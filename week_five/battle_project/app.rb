648585f0bd4fb727ae4ac337fb64ccf0dc05f24f
LinTrieu <36490540+LinTrieu@users.noreply.github.com>
2019-05-29 14:48:09 +0100

    \ 'Introducing Params' exercise

--- week_five/battle_project/app.rb
+++ week_five/battle_project/app.rb
@@ -16,7 +16,13 @@ get '/contact' do
   "Contact Us"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params[:name]
+  @name = params[:name]
   erb(:index)
 end
