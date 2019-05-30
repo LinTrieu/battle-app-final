00628382acf99ae5d6ab741d5aeb00a815a5e5f6
adrem7 <adrian.raisin@gmail.com>
2019-05-30 16:05:04 +0100

    Redirect and sessions

--- week_five/battle_project/battle/app.rb
+++ week_five/battle_project/battle/app.rb
@@ -1,16 +1,29 @@
 require 'sinatra/base'
 
-  class Battle < Sinatra::Base
+class Battle < Sinatra::Base
 
-    get "/" do
-      erb(:index)
-    end
+  enable :sessions
 
-    post "/names" do
-      @player_one_name = params[:player_one]
-      @player_two_name = params[:player_two]
-      erb(:play)
-    end
-
-    run! if app_file == $0
+  get "/" do
+    erb(:index)
   end
+
+  post "/names" do
+    session[:player_one] = params['player_one']
+    session[:player_two] = params['player_two']
+    redirect '/play'
+  end
+
+
+
+  get "/play" do
+    # "player_one = " << session[:player_one].inspect
+    # "player_two = " << session[:player_two].inspect
+
+    @player_one_name = session[:player_one]
+    @player_two_name = session[:player_two]
+    erb(:play)
+  end
+
+  run! if app_file == $0
+end
---
+++ week_five/battle_project/battle/spec/features/enter_names_spec.rb
@@ -0,0 +1,14 @@
+require 'capybara/rspec'
+require 'spec_helper'
+require './app'
+
+feature 'testing homepge' do
+
+  scenario 'users input names into forms and submit to display names' do
+    visit("/")
+    fill_in :player_one, with: "player_one_name"
+    fill_in :player_two, with: "player_two_name"
+    click_on "Submit"
+    expect(page).to have_content "player_one = player_one_name player_two = player_two_name"
+  end
+end
