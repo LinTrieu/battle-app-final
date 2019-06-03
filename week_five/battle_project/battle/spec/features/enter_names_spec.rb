f41af081ca9026cfce651d7fc4f5702a1ad01141
LinTrieu <36490540+LinTrieu@users.noreply.github.com>
2019-06-03 23:20:36 +0100

    DRY codebase with web_helpers.rb

--- week_five/battle_project/battle/spec/features/enter_names_spec.rb
+++ week_five/battle_project/battle/spec/features/enter_names_spec.rb
@@ -5,10 +5,7 @@ require './app'
 feature 'testing homepage' do
 
   scenario 'users input names into forms and submit to display names' do
-    visit("/")
-    fill_in :player_one, with: "Lin"
-    fill_in :player_two, with: "Dave"
-    click_on "Submit"
+    sign_in_and_play
     expect(page).to have_content "Lin vs. Dave"
   end
 end
--- week_five/battle_project/battle/spec/features/hit_points_spec.rb
+++ week_five/battle_project/battle/spec/features/hit_points_spec.rb
@@ -5,10 +5,7 @@ require './app'
 feature 'View player hit points' do
 
   scenario 'player one views player two\'s points' do
-    visit("/")
-    fill_in :player_one, with: "Lin"
-    fill_in :player_two, with: "Dave"
-    click_on "Submit"
+    sign_in_and_play
     expect(page).to have_content "Dave: 10 HP (Hit Points)"
   end
   
